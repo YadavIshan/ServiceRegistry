d96b26c73058afa884fce66862b103e5851d5925
Ishan Yadav <37396882+YadavIshan@users.noreply.github.com>
2026-01-30 23:56:01 +0530

    Initial code commit

---
+++ src/main/java/com/ishan/ecommerce/ServiceRegistry/ServiceRegistryApplication.java
@@ -0,0 +1,18 @@
+package com.ishan.ecommerce.ServiceRegistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import io.github.cdimascio.dotenv.Dotenv;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+		SpringApplication.run(ServiceRegistryApplication.class, args);
+	}
+
+}
